33dfe53116f8109ef17feac9f826a9f3610f3e2c
Bradley Nixon <bnixon@xmhawaii.com>
2021-11-01 09:15:11 -0700

    added api URL

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://56fb6f0a.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
